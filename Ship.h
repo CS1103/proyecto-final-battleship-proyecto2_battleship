adab8e4bc9ada66e31cea9cc6c731716039e357b
ivan mamani <46487626+ivaniuss@users.noreply.github.com>
2019-11-26 17:35:49 -0500

    Add files via upload

---
+++ Ship.h
@@ -0,0 +1,28 @@
+#ifndef PROYECTOBATTLESHIP_SHIP_H
+#define PROYECTOBATTLESHIP_SHIP_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+class Ship {
+private:
+    int size;
+    int damage;
+    char orientation;
+    int cx;
+    int cy;
+    string name;
+public:
+    Ship(int tamano, int dano, string nombre);
+    void dmg();
+    int getsize();
+    string getname();
+    bool check(string **array, int area);
+    void setship(string **array, int area);
+    void setcomputership(string **array, int area);
+    void locate(string **array, int area);
+    void locatecomputer(string **array, int area);
+};
+#endif //PROYECTOBATTLESHIP_SHIP_H
